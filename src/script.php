849217291ac9a1db7216770c9bc99298558f763a
Alexandre <prezio.alexandre@gmail.com>
2019-01-11 16:10:03 +0100

    ta gueule

--- src/script.php
+++ src/script.php
@@ -12,7 +12,7 @@ use TestPHP\Invoice;
 use TestPHP\InvoiceLine;
 
 $l1 = new InvoiceLine(20,'Art1',20);
-$l2 = new InvoiceLine(25,'Art2',20);
+$l2 = new InvoiceLine(30,'Art2',20);
 $l3 = new InvoiceLine(20,'Art3',20);
 
 $tablist = [$l1,$l2,$l3];
